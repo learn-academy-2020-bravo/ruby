a2e7483881e6bd0dbc9f1565ada45b658f8b2980
chrissie-akau <cakau9@gmail.com>
2020-05-19 14:52:47 -0700

    All DONE

--- ruby-class-chrissie-ara.rb
+++ ruby-class-chrissie-ara.rb
@@ -9,22 +9,54 @@
 #
 # Story: As a developer, when I print a Task that is done, its status is shown.
 
+# class Task
+#   def initialize ()
+#     @chore1 = "dishes"
+#     @chore2 = "vacuuming"
+#     @chore3 = "laundry"
+#   end
+
+#   def get_task
+#     "#{@chore1} #{@chore3} needs to be done!"
+#   end
+
+#   def done_task
+#     "#{@chore2} is done!"
+#   end
+# end
+
+# kitchen = Task.new
+# p kitchen.get_task
+# p kitchen.done_task
+
 class Task
-  def initialize
-    @chore1 = "dishes"
-    @chore2 = "vacuuming"
-    @chore3 = "laundry"
-  end
+    def initialize (chore, description)
+      @chore = chore
+      @description = description
+      @status = "needs to be done still!"
+    end
+  
+    def get_chore
+      @chore
+    end
 
-  def get_task
-    "#{@chore1} #{@chore3} needs to be done!"
-  end
+    def get_description
+      "#{@chore} is on the to do list and #{@description} and #{@status}. "
+    end
 
-  def done_task
-    "#{@chore2} is done!"
-  end
+    def get_status
+      @status = "#{@chore} is all done!"
+    end
 end
 
-kitchen = Task.new
-p kitchen.get_task
-p kitchen.done_task
+laundry = Task.new("laundry", "put clothes into machine")
+p laundry.get_description
+p laundry.get_status
+
+dishes = Task.new("Dishes", "put in dishwasher")
+p dishes.get_status
+
+sweeping = Task.new("Sweeping", "needs to be sweeped")
+p sweeping.get_description
+
+    
