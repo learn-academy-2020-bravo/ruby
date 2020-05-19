6bdafe089dba18a1a6329deb9868ca5e66a4226b
chrissie-akau <cakau9@gmail.com>
2020-05-19 13:51:40 -0700

    FIZZBUZZ ADDED

--- ruby-blocks-ara-chrissie.rb
+++ ruby-blocks-ara-chrissie.rb
@@ -142,3 +142,19 @@ while num <= 99
     p num
   end
 end
+#OR
+def fizzbuzz number
+  (1..number).each do |i|
+    if i % 3 == 0 && i % 5 == 0
+      puts 'fizzbuzz'
+    elsif i % 3 == 0
+      puts 'fizz'
+    elsif i % 5 == 0
+      puts 'buzz'
+    else
+      puts i
+    end
+  end
+end
+
+fizzbuzz(100)
