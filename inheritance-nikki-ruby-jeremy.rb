f5ac84e32299cba0a585546d752f3747f0993b71
nbrooks89 <nbrooks2389@gmail.com>
2020-05-20 10:55:45 -0700

    asd

--- inheritance-nikki-ruby-jeremy.rb
+++ inheritance-nikki-ruby-jeremy.rb
@@ -4,7 +4,24 @@ class Car
     @model = model
     @year = year
     @wheels = 4
+    @lights = "off"
+    @turn_signal = "off"
+    @speed = 0
   end
+
+  def light_switch
+    @lights = "on"
+  end
+
+  def signal_left
+    @turn_signal = "turn left"
+  end
+
+  def signal_right
+    @turn_signal = "turn right"
+  end
+
+
 
   def wheels
     @wheels
@@ -13,3 +30,14 @@ end
 
 my_car = Car.new("generic",1999)
 p my_car.wheels
+
+my_tesla = Car.new("x2020", 2021)
+
+
+
+my_toyota = Car.new("camery", 1997)
+
+
+
+
+my_honda = Car.new("accord", 1889)
