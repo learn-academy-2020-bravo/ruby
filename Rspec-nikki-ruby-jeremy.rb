a2ae91883fdd743dccf5f417834ac2755dc4405e
Jeremy Gabriel <jeremyagabriel@gmail.com>
2020-05-20 16:54:55 -0700

    RESPECT

--- Rspec-nikki-ruby-jeremy.rb
+++ Rspec-nikki-ruby-jeremy.rb
@@ -18,13 +18,38 @@ class Task
     end
 
     def status
-        @status 
-    end  
-    
+        @status
+    end
+
     def show_status
        p "#{@name} is #{@status}"
     end
 end
 
+class TaskList
+  def initialize
+    super()
+      @tasklist = []
+  end
+
+  def tasklist
+    @tasklist
+  end
+
+  def tasklist_completed
+    tasklist.select do |value|
+      value.status == "done"
+    end.map do |value|
+      value.name
+    end
+  end
+
+end
+
 laundry = Task.new "laundry","dirty clothes in washer"
-  
+dishes = Task.new "dishes","wash dishes"
+my_tasklist = TaskList.new
+my_tasklist.tasklist << laundry
+my_tasklist.tasklist << dishes
+
+p my_tasklist.tasklist
--- Rspec-nikki-ruby-jeremy_spec.rb
+++ Rspec-nikki-ruby-jeremy_spec.rb
@@ -9,7 +9,7 @@ describe Task do
 
     it 'i can give an instance a title and retrieve it' do
         laundry = Task.new 'laundry', "dirty clothes in washer"
-        expect(laundry.name).to eq 'laundry' 
+        expect(laundry.name).to eq 'laundry'
     end
 
     it 'i can give an instance a description and retrieve it' do
@@ -20,16 +20,33 @@ describe Task do
     it 'it initializes a task in progress unless a task is done' do
         laundry = Task.new 'laundry', "dirty clothes in washer"
         expect(laundry.status).to eq "in progress"
-        laundry.done 
+        laundry.done
         expect(laundry.status).to eq "done"
     end
-    
+
     it 'when I print a Task that is done, its status is shown' do
         laundry = Task.new 'laundry', "dirty clothes in washer"
         laundry.done
         expect(laundry.show_status).to eq "laundry is done"
     end
+
+    it 'Can add all tasks to a TaskList' do
+      laundry = Task.new "laundry","dirty clothes in washer"
+      dishes = Task.new "dishes","wash dishes"
+      my_tasklist = TaskList.new
+      my_tasklist.tasklist << laundry
+      my_tasklist.tasklist << dishes
+      expect(my_tasklist.tasklist).to eq [laundry, dishes]
+    end
+
+    it 'Can print array of tasks with status done' do
+      laundry = Task.new "laundry","dirty clothes in washer"
+      dishes = Task.new "dishes","wash dishes"
+      my_tasklist = TaskList.new
+      laundry.done
+      my_tasklist.tasklist << laundry
+      my_tasklist.tasklist << dishes
+      expect(my_tasklist.tasklist_completed).to eq ["laundry"]
+    end
+
 end
-
-
-    
