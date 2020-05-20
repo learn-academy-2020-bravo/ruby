e181da57a8b10f29e6fc7419e1ad319304d0a3fe
aaron masanes <aaronmasanes@hotmail.com>
2020-05-20 11:24:31 -0700

    done comments added

--- inheritance-thomas-aaron.rb
+++ inheritance-thomas-aaron.rb
@@ -1,5 +1,5 @@
-class Car
-  def initialize
+ class Car #this is our base/parent class 
+  def initialize #similiar to state components basically variables 
     @model = "generic car"
     @year = "my_car year"
     @lights = "Off"
@@ -7,7 +7,8 @@ class Car
     @speed = 0
     @wheels = 4
   end
-  def model
+#these are all display methods
+  def model 
     @model
   end
   
@@ -30,18 +31,18 @@ class Car
   def speed
     @speed
   end
-
+   #changes the light switch variable everytime its called 
   def light_switch
     if (@lights == "On")
       @lights = "Off"
     else @lights = "On"
     end
   end
-
+# turn signal is given whatever value passed into it
   def turn_signal_switch signal
     @turn_signal = signal
   end
-
+# string interpolation uses all variables from init to give us the current state of the car
   def car_info
     puts "This car's model is #{@model}.  Its year is #{@year}.  The lights are #{@lights}.  The turn signal is #{@turn_signal}.  It is currently going #{@speed} mph.  It has #{@wheels} wheels."
   end
@@ -50,17 +51,18 @@ end
 # my_car = Car.new
 # puts my_car.wheels
 
-class Tesla < Car
+
+class Tesla < Car #retrieves variables not declared in tesla so it gets wheels etc..
   def initialize (model, year)
-    super()
+    super() #ask instructor or look up
     @model = model
     @year = year
   end
-
+  #this is specific to the child class
   def accelerate
     @speed += 10
   end
-
+  #this is specific to the child class
   def brake
     @speed -= 7
     if (@speed < 0)
@@ -68,7 +70,7 @@ class Tesla < Car
     end
   end
 end
-
+   
    my_tesla = Tesla.new('tesla', '2015')
    my_tesla.light_switch
  # puts my_tesla.lights
@@ -85,13 +87,14 @@ end
  # puts my_tesla.speed
    my_tesla.car_info
 
+   #inheriting from Car
 class Toyota < Car
   def initialize (model, year)
     super()
     @model = model
     @year = year
   end
-
+  #unique to Toyota
   def accelerate
     @speed += 7
   end
@@ -105,10 +108,10 @@ class Toyota < Car
 end
 
 
-
+#inherits variables from parent class of var
 class Honda < Car
   def initialize (model, year)
-    super()
+    super() #we need model and year because its specific to Honda, it overrides parent values 
     @model = model
     @year = year
   end
@@ -125,12 +128,13 @@ class Honda < Car
   end
 end
 
+#we can add to our collection based on model and year
 class Collection 
   def initialize
     @car_model = []
     @car_years = []
   end
-
+  #shovels cars into the array
   def car_years cars
     @car_years << cars
   end
@@ -146,11 +150,11 @@ class Collection
   def cars 
     @car_model
   end
-
+  # sorts car by the year
   def sort_cars
     @car_years = @car_years.sort
   end
-
+ 
   def sort_alpha
     @car_model = @car_model.sort
   end
