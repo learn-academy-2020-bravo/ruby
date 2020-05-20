3f12b7e685316c579493b81a07ae2a9d4aff76e8
Ara Jo <arajodeveloper@gmail.com>
2020-05-20 11:56:19 -0700

    done

---
+++ car-challenge-jazmyn-ara.rb
@@ -0,0 +1,216 @@
+# Story: As a programmer, I can make a car. Hint: Create a class called Car, and create a variable called my_car which contains an object of class Car.
+#
+
+class Car
+
+  def initialize model, year
+    @model = model
+    @year = year
+    @wheels = 4
+    @lights = "OFF"
+    @signals = "OFF"
+    @speed = 0
+  end
+
+  def model
+    @model
+  end
+
+  def year
+    @year
+  end
+
+  def wheels
+    @wheels
+  end
+
+  def lights
+    @lights
+  end
+
+  def signals
+    @signals
+  end
+
+  def speed
+    @speed
+  end
+
+  def change_lights
+    if @lights == "ON"
+      @lights = "OFF"
+    else
+      @lights = "ON"
+    end
+  end
+
+  def change_signals_right
+    if @signals == "OFF"
+      @signals = "RIGHT ON"
+    else
+      @signals = "OFF"
+    end
+  end
+
+  def change_signals_left
+    if @signals == "OFF"
+      @signals = "LEFT ON"
+    else
+      @signals = "OFF"
+    end
+  end
+
+  def show_info
+    "The car is #{@model} and #{@year} has #{@wheels} wheels. The lights are #{@lights}. Now turning #{@signals}."
+  end
+
+
+end
+
+my_car = Car.new 'bmw','1988'
+p my_car.show_info
+
+
+
+
+
+class Tesla < Car
+  def initialize model, year
+    super(model, year)
+  end
+
+
+  def show_tesla_info
+    "#{@model} is from #{@year} and has #{@wheels} wheels. The lights are #{@lights}. The speed is #{@speed}."
+  end
+
+
+  def speed_up
+    @speed += 10
+  end
+
+  def speed_down
+    @speed -= 7
+    if @speed < 0
+      @speed = 0
+    end
+  end
+
+
+end
+
+p my_tesla = Tesla.new('Model3','2018')
+p my_tesla.to_s
+# my_tesla.speed_up
+# my_tesla.speed_up
+# my_tesla.speed_down
+# my_tesla.speed_down
+# my_tesla.speed_down
+# # p my_tesla.show_tesla_info
+# my_tesla.change_lights()
+# p my_tesla.show_tesla_info
+
+
+class Toyota  < Car
+  def initialize model, year
+    super
+  end
+
+  def show_toyota_info
+    "#{@model} is from #{@year} and has #{@wheels} wheels."
+  end
+
+end
+
+my_toyota = Toyota.new 'Camry','2005'
+# p my_toyota.show_toyota_info
+
+class Honda < Car
+  def initialize model, year
+    super
+  end
+
+  def show_honda_info
+    "#{@model} is from #{@year} and has #{@wheels} wheels."
+  end
+
+end
+
+my_honda1 = Honda.new 'Civic','2004'
+my_honda2 = Honda.new 'Civic','2005'
+my_honda3 = Honda.new 'Civic','2006'
+my_honda4 = Honda.new 'Civic','2007'
+my_honda5 = Honda.new 'Civic','2008'
+# p my_honda.show_honda_info
+
+class Garage
+  def initialize
+    @car_collection = []
+  end
+
+  def add_cars
+    @car_collection
+  end
+end
+
+car_1 = Garage.new
+car_1.add_cars << my_honda1.show_honda_info
+car_1.add_cars << my_honda2.show_honda_info
+car_1.add_cars << my_honda3.show_honda_info
+
+p car_1.add_cars
+
+
+
+
+
+# Story: As a programmer, I can give my car a model on initialization. Hint: The model for the car class can be "generic car"
+#
+# Story: As a programmer, I can give my car a year on initialization. Hint: The year for the car class can be "my_car year"
+#
+# Story: As a programmer, I can tell how many wheels a car has. The default is four. Hint: initialize the car to have four wheels, then create a method to return the number of wheels.
+#
+# Story: As a programmer, I can make a Tesla car. Hint: Create an variable called my_tesla which is an instance of class Tesla which inherits from class Car.
+#
+# Story: As a programmer, I can give my Tesla a model on initialization.
+#
+# Story: As a programmer, I can give my Tesla a year on initialization.
+#
+# Story: As a programmer, I can make a Toyota car.
+#
+# Story: As a programmer, I can give my Toyota a model on initialization.
+#
+# Story: As a programmer, I can give my Toyota a year on initialization.
+#
+# Story: As a programmer, I can make a Honda car.
+#
+# Story: As a programmer, I can give my Honda a model on initialization.
+#
+# Story: As a programmer, I can give my Honda a year on initialization.
+#
+# Story: As a programmer, I can turn on and off the lights in my cars. Lights start in the off position.
+#
+# Story: As a programmer, I can signal left and right. Turn signals starts off.
+#
+# Story: As a programmer, I can determine the speed of a car. Speed starts at 0 mph.
+#
+# Story: As a programmer, I can speed my Teslas up by 10 per acceleration.
+#
+# Story: As a programmer, I can slow my Teslas down by 7 per braking.
+#
+# Story: As a programmer, I can speed my Toyotas up by 7 per acceleration.
+#
+# Story: As a programmer, I can slow my Toyotas down by 5 per braking.
+#
+# Story: As a programmer, I can speed my Hondas up by 5 per acceleration.
+#
+# Story: As a programmer, I can slow my Hondas down by 2 per braking.
+#
+# Story: As a programmer, I can call upon a car to tell me all its information. Hint: Implement to_s on one or more classes. You can call a super class's to_s with super.
+#
+# Stretch Challenges
+# Story: As a programmer, I can keep a collection of two of each kind of car model, all from different years. Hint: Create two of each vehicles, all from different model years, and put them into an array.
+#
+# Story: As a programmer, I can sort my collection of cars based on model.
+#
+# Story: As a programmer, I can sort my collection of cars based on year. Hint: Find out how the spaceship operator can help you with an array.
